908d16437ebd1e53c9f74ae76bfd6230b4d0b342
dchristofolli <dchristofolli@grupodimed.com.br>
2020-02-24 13:16:42 -0300

    projeto limpo

--- src/main/java/br/com/dchristofolli/kafka/producer/EventProducer.java
+++ src/main/java/br/com/dchristofolli/kafka/producer/EventProducer.java
@@ -1,7 +1,6 @@
 package br.com.dchristofolli.kafka.producer;
 
 
-import com.google.gson.Gson;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.producer.ProducerRecord;
@@ -13,9 +12,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class EventProducer {
     private KafkaTemplate<String, Object> kafkaTemplate;
-    private Gson gson;
 
     public void send(String topic, ProducerRecord<String, Object> producerRecord) {
-        kafkaTemplate.send(topic, gson.toJson(producerRecord));
     }
 }
